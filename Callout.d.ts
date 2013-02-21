ebbf595c54d1bb506e5ddd4f14e9154bf1305377
Charlie Holland <chahol@lineone.net>
2013-02-21 20:30:57 +0000

    Added CalloutActions etc

--- Callout.d.ts
+++ Callout.d.ts
@@ -1,6 +1,5 @@
 //TypeScript definitions for Callout.js in SharePoint 2013
 
-
 declare class CalloutOptions {
     ID: string;
     launchPoint: HTMLElement;
@@ -42,8 +41,8 @@ declare class Callout {
     isOpenOrOpening(): bool;
     isClosing(): bool;
     isClosed(): bool;
-    getActionMenu(): any;
-    addAction(any);
+    getActionMenu(): CalloutActionMenu;
+    addAction(action:CalloutAction);
     refreshActions():void;
     open(useAnimation: bool);
     close(useAnimation: bool);
@@ -51,6 +50,44 @@ declare class Callout {
     destroy(): void;
 }
 
+declare class CalloutActionOptions {
+    text: string;
+    tooltip: string;
+    disabledTooltip: string;
+    onClickCallback: (event,action:CalloutAction)=>any;
+    isEnabledCallback: any;
+    isVisibleCallback: any;
+    menuEntries: CalloutActionMenuEntry[];
+}
+
+declare class CalloutActionMenuEntry {
+    constructor(text, onClickCallback, wzISrc, wzIAlt, wzISeq, wzDesc);
+}
+
+declare class CalloutActionMenu {
+    constructor(actionsId);
+    addAction(action: CalloutAction);
+    getActions(): CalloutAction[];
+    render():void;
+    refreshActions():void;
+    calculateActionWidth():void;
+}
+
+declare class CalloutAction {
+    constructor(options: CalloutActionOptions);
+    getText(): string;
+    getToolTop(): string;
+    getDisabledToolTip(): string;
+    getOnClickCallback(): any;
+    getIsDisabledCallback(): any;
+    getIsVisibleCallback(): any;
+    getIsMenu(): bool;
+    getMenuEntires(): any;
+    render():void;
+    isEnabled(): bool;
+    isVisible(): bool;
+    set (options: CalloutActionOptions):void;
+}
 
 interface ICalloutManager {
     createNew(options: CalloutOptions): Callout;
